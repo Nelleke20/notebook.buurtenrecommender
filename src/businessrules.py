8b511ab6ebd375057cef9431104bf33f6c00df49
Nelleke Smits <nelleke_s@hotmail.com>
2022-12-29 15:00:28 +0100

    fix formatting

--- src/businessrules.py
+++ src/businessrules.py
@@ -1,5 +1,6 @@
 import logging
 
+
 class BusinessRules:
     """
     Define business rules to apply to dataframe.
@@ -9,23 +10,26 @@ class BusinessRules:
         determine if business rules apply to this item.
 
     """
+
     def __init__(self):
         pass
 
-    def apply_business_rules(self, X):       
-        logging.info('first we will apply the business-rules for all the cases (where possible)...')
+    def apply_business_rules(self, X):
+        logging.info(
+            "first we will apply the business-rules for all the cases (where possible)..."
+        )
 
         # business-rule 1: check if feature one is bigger than
         if X[0][0] > 350:
-            logging.info('based on business rule 1')
+            logging.info("based on business rule 1")
             return 2000
 
         # business-rule 2: check if feature two is bigger than
         elif X[0][1] > 12:
-            logging.info('based on business rule 2')
+            logging.info("based on business rule 2")
             return 2000
 
         # when no business-rule is applicable, we pass it to the ML-model
         else:
-            logging.info('no business-rule applicable..')
+            logging.info("no business-rule applicable..")
             pass
