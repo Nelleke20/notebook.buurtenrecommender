ecc2aff83e94ed77898526002cc462e53d6f24a5
Nelleke Smits <nelleke_s@hotmail.com>
2023-12-24 18:13:29 +0100

    feat(streamlit): add working streamlit frontend and fastapi backend

---
+++ old/src/main.py
@@ -0,0 +1,58 @@
+import businessrules as rules
+import joblib
+import logging
+
+
+class BuurtenModel(rules.BusinessRulesOne, rules.BusinessRulesTwo):
+    def __init__(self):
+        self._model_one = joblib.load("../data/finalized_model.sav")
+        self._model_two = joblib.load("../data/finalized_model.sav")
+
+    def prepare_dataset(self, X):
+        logging.info("preparing the dataset...")
+        # prepare dataset
+        X = X
+        return X
+
+    def apply_business_rules_one(self, X):
+        # business-rules.py  is used to define business rules
+        return super().apply_business_rules_one(X)
+
+    def apply_business_rules_two(self, X, output_one):
+        # business-rules.py  is used to define business rules
+        return super().apply_business_rules_two(X, output_one)
+
+    def apply_model_one(self, X):
+        logging.info("second we will apply the ml-model for the hard-cases...")
+        # prediction_type = 'model_voorspelling'
+        output = self._model_one.predict(X)
+        return output[0]
+
+    def apply_model_two(self, X):
+        logging.info("second we will apply the ml-model for the hard-cases...")
+        output = self._model_two.predict(X)
+        return output[0]
+
+    def predict(self, X):
+
+        # PREP: prepare dataset
+        X_prep = self.prepare_dataset(X)
+
+        # PART ONE: apply first business rules and model
+        output_step_one = self.apply_business_rules_one(X_prep)
+        output_step_one["prediction_one"] = [
+            output_step_one["prediction_one"]
+            if output_step_one["prediction_type_one"] != "model_voorspelling"
+            else self.apply_model_one(X_prep)
+        ]
+
+        # PART TWO: Apply business rules en model for part two
+        output_step_two = self.apply_business_rules_two(X_prep, output_step_one)
+
+        output_step_two["prediction_two"] = [
+            output_step_two["prediction_two"]
+            if output_step_two["prediction_type_two"] != "model_voorspelling"
+            else self.apply_model_two(X_prep)
+        ][0]
+
+        return output_step_two
