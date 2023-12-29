70eac5523f9c36c16f393b90b0b5c8007aba5da3
Nelleke Smits <nelleke_s@hotmail.com>
2023-12-29 13:01:55 +0100

    fix(streamlit): edit size of map for phone

--- demo/demoapp.py
+++ demo/demoapp.py
@@ -29,7 +29,7 @@ if buurt != '<select>':
 
         if html_map:
             st.markdown('Viola, de volgende 3 buurten lijken op jouw favoriet: ')
-            components.html(html_map, height=300, width=400) #600 and 700 for web
+            components.html(html_map, height=350, width=350) #600 and 700 for web
 else:
     st.markdown('Wat is jouw favoriete buurt? Selecteer je buurt en de vergelijker doet zijn werk...')
     # st.image(image=img, caption="Buurten Vergelijker")
